c400a6875a8aa8b896c6dbb445b13fe16108e5c7
Working Tech <k2785233247@gmail.com>
2026-06-27 11:56:07 +0800

    尝试实践了头文件, 遇到了一些不解的问题, 正在尝试解决

--- C_Reboot/1.Headers/main.c
+++ C_Reboot/1.Headers/main.c
@@ -1,1 +1,12 @@
-// 研究一下'头文件'
+#include<stdio.h> // 从'系统C库'中引入printf
+#include"my_operations.h" // 从'当前目录'引入自定义的'头文件'
+
+// int value_in_header=114;
+int main(){
+    // printf("The value provided in the header file is: %d\n", value_in_header);
+    int a=5, b=4;
+    printf("Value a = %d, b = %d\n",a,b);
+    // printf("a + b = %d\n", num_sum(a,b));
+    printf("a - b = %d\n", num_sub(a,b));
+    return 0;
+}
---
+++ C_Reboot/1.Headers/op_implementation.c
@@ -0,0 +1,11 @@
+#include "my_operations.h" // 引入
+// Tips: 此处用的是"双引号"引入, 表示从 当前路径/相对目录 下引入文件, 而<尖括号>则是从"系统C库"中引入
+
+// 
+int value_in_header = 514; 
+int num_sum(int a, int b){
+    return a+b;
+} 
+// int num_sub(int a, int b){
+//     return 2*a-b;
+// }
