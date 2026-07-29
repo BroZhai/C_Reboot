8b39b04bf8ae6e33557f7e1fc7218678cba88d6e
Working Tech <k2785233247@gmail.com>
2026-07-29 13:02:54 +0800

    简单实践了main函数通过指针数组读命令行参数, 了解了一些小细节

--- C_Reboot/Basics/10.Pointers/PointerArray/special_main.c
+++ C_Reboot/Basics/10.Pointers/PointerArray/special_main.c
@@ -7,5 +7,18 @@
 // char* argv[] 指针数组: 因为命令行每个输入的指令'长度不确定', 但都是char, 这里便可以用 char*指针数组来 灵活分配内存给这些指令 & 灵活调用它们
 
 int main(int argc, char* argv[]){
-    
+    if(argc < 3){
+        printf("Please provide 3 or more command input! Seperated by SPACE!\n");
+        return 0;
+    }
+    printf("The argc count is: %d \n", argc);
+    PrintLine;
+
+    printf("The first command: %s\n", *(argv)); 
+    printf("The second command: %s\n", *(argv+1));
+    printf("The third command: %s\n", *(argv+2));
+    // printf("The pointer address to Last command: %p \n", (void*)&argv[argc-1]);  // 指针指向最后的'内容地址'
+    // printf("The content of argv[argc-1] is: %p \n", (void*)argv[argc-1]); // 最后一个内容 (所表示的'地址')
+    // printf("The dereferenced content of argv[argc-1] is: %c \n", 8(argv[argc-1])); // 最后一个内容地址的'解引用', 得内容的值
+    printf("The LAST command is: %s \n", argv[argc-1]);
 }
