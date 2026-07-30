789fb729a7f7d09b6466f756f035d7c0ef0f6732
Working Tech <k2785233247@gmail.com>
2026-07-30 11:00:41 +0800

    简单的看了下函数返回指针, 一会儿继续

--- C_Reboot/Basics/10.Pointers/PointerInFunction/main.c
+++ C_Reboot/Basics/10.Pointers/PointerInFunction/main.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 #define PrintLine printf("\n")
 
 // 对传入的指针数组的'最后一个'地址所指向的值 + 1, 直接应作用在原指针数组上
 void last_plus_one(int* pointer_array[], int array_size){
-    // 取到最后一个元素的地址, 解引用 再 +1 即可
+    // 取到最后一个元素的地址, 直接解引用得变量/变量值, 再 +1 即可
     *(pointer_array[array_size-1]) = *(pointer_array[array_size-1])+1;
 }
 
@@ -13,6 +15,15 @@ void show_all_values(int* pointer_array[], int array_size){
         printf("%d, ",*(pointer_array[i]));
     }
     PrintLine;
+}
+
+char* make_string(const char* src) {
+    char* copy = malloc(strlen(src) + 1); // malloc(想要申请多少Byte), +1 是因为strlen()不会考虑'\0'
+    printf("The incoming string address is: %p, which the dereferenced value is: %c \n", (void*)src, *src);
+    strcpy(copy, src); // strcpy(新起始地址, 原字符串起始地址), 随后实现"内容照搬"
+    printf("The allocated starting memory address is: %p, which the dereferenced value is: %c \n", (void*)copy, *copy);
+    PrintLine;
+    return copy;  // 返回调用者拿到后负责 free
 }
 
 int main(){
@@ -29,4 +40,13 @@ int main(){
 
     show_all_values(num_array, array_size);
     last_plus_one(num_array, array_size);
+    show_all_values(num_array, array_size);
+    PrintLine;
+
+    // 从函数返回指针的示例见 8.Array/ReturnArrayInFunction/return_arr.c :)
+    // 这里我们用另外一个例子: 返回在函数中'灵活分配'的内存地址
+    char* my_str_ptr;
+    my_str_ptr = make_string("Pancake is Big BAKA");
+    printf("Returned to main...\n");
+    printf("The memory address of my_str_ptr is: %p, which the dereferenced value is: %c \n", (void*)my_str_ptr, *my_str_ptr);
 }
