a00031cb0ea642832ae6e3d5a465ca7366b94cd6
Working Tech <k2785233247@gmail.com>
2026-07-31 13:48:20 +0800

    追加了一些其他的指针更新实验

--- C_Reboot/Basics/10.Pointers/redirection.c
+++ C_Reboot/Basics/10.Pointers/redirection.c
@@ -6,11 +6,22 @@
 int main(){
     // 创建一个指针, 查看其当前指向的内存地址
     char* string = "Hello, this is yellocake :)";
-    printf("The address of string is now: %p, and the content is: %s", (void*)string, string);
-    PrintLine;
+    printf("The address of string is now: %p, and the content is: %s\n", (void*)string, string);
 
     // 重新对指针赋值, 一般来说, 指针会指向'新内容'的地址
-    // string = "Hello, this is yellocake :)"; // 但当'新内容'和原来一样时, 编译器会很聪明的'保持不变' (内容都一样嘛, 为什么我要新建个内存存两个一摸一样的东西 :3)
+    string = "Hello, this is yellocake :)"; // 但当'新内容'和原来一样时, 编译器会很聪明的'保持不变' (内容都一样嘛, 为什么我要新建个内存存两个一摸一样的东西 :3)
+    printf("The address of string is now: %p, and the content is: %s\n", (void*)string, string);
     string = "Hello, this is yellocake"; // 和'原内容'不一样, 此时string将会指向该'新内容'的起始地址, '原内容'就无人指认了 XD
-    printf("The address of string is now: %p, and the content is: %s", (void*)string, string);
+    printf("The address of string is now: %p, and the content is: %s\n", (void*)string, string);
+    PrintLine;
+
+    // 看看其他类型的指针
+    int a=114, b=114;
+    int c = b;
+    int* ptr = &a;
+    printf("The ptr is now pointing to: %p \n", (void*)ptr);
+    ptr = &b;
+    printf("The ptr is now pointing to: %p \n", (void*)ptr);
+    ptr = &c;
+    printf("The ptr is now pointing to: %p \n", (void*)ptr);
 }
