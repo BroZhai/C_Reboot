93010e008717d1cb452234d90b5670ed8f428813
Working Tech <k2785233247@gmail.com>
2026-08-05 13:48:02 +0800

    补上了一个关键注释

--- C_Reboot/Basics/10.Pointers/ManualShift/main.c
+++ C_Reboot/Basics/10.Pointers/ManualShift/main.c
@@ -15,6 +15,8 @@ int main(){
     int arr[] = {1,2,3,4,5};
     char* char_ptr_to_arr = (char*)arr; // 此处进行强制类型转换, char*指针展开后只会按 1Byte (sizeof(char))偏移
     int* int_ptr_to_arr = arr;
-    printf("Accessing thrid element using char*: %d\n", *(char_ptr_to_arr + 8)); // 等效于 *(ptr_to_arr + 8 x sizeof(char)), sizeof(char)为 1Byte, 8 x 1
+    printf("Accessing thrid element using char*: %d\n", *(int*) (char_ptr_to_arr + 8)); // 等效于 *(ptr_to_arr + 8 x sizeof(char)), sizeof(char)为 1Byte, 8 x 1
+    // 这里虽然是按'char *'进行的地址偏移, 但实际解引用的值还是int, 所以还要用(int*)强转回int指针进行正常的'解引用'
+    
     printf("Accessing thrid element using int*: %d\n", *(int_ptr_to_arr + 2));// 等效于*(ptr + 2 x sizeof(int)), sizeof(int) 为 4 Byte, 2 x 4
 }
